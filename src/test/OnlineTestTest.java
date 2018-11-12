e3ebf37e8a6ad28bd34e62e2562a5939f0e4b976
SKADAD <32932385+SKADAD@users.noreply.github.com>
2018-11-12 14:28:31 +0100

    rebase (#18)
    * Test package and file structure redone as template
    
    * class public added
    
    * Testpush
    
    * Pushing tests
    
    * Commit
    
    * Commit
    
    * rebase (#4)
    
    * Testpush
    
    * Pushing tests
    
    * Junit change 1
    
    * Pushing tests
    
    * class public added
    
    * class public added (#5)
    
    * Merge branch 'master' of https://github.com/KimThomasEriksson/CI-CD into buoc1701
    
    # Conflicts:
    #	src/test/OnlineTestTest.java
    
    * Small changes (#16)
    
    * Merge branch 'master' of https://github.com/KimThomasEriksson/CI-CD into buoc1701
    
    # Conflicts:
    #	src/test/OnlineTestTest.java
    
    * Merge branch 'master' of https://github.com/KimThomasEriksson/CI-CD into buoc1701
    
    # Conflicts:
    #	src/test/OnlineTestTest.java

--- src/test/OnlineTestTest.java
+++ src/test/OnlineTestTest.java
@@ -22,9 +22,148 @@ class OnlineTestTest {
     @org.junit.jupiter.api.Test
     void check() {
 
+        // Testa #1 fråga
+        myApp.current = 0;
+        myApp.jb[0].setSelected(true);
+        Assertions.assertFalse(myApp.check());
         myApp.current = 0;
         myApp.jb[1].setSelected(true);
         Assertions.assertTrue(myApp.check());
+        myApp.current = 0;
+        myApp.jb[2].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 0;
+        myApp.jb[3].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+
+        // Testa #2 fråga
+        myApp.current = 1;
+        myApp.jb[0].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 1;
+        myApp.jb[1].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 1;
+        myApp.jb[2].setSelected(true);
+        Assertions.assertTrue(myApp.check());
+        myApp.current = 1;
+        myApp.jb[3].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+
+        // Testa #3 fråga
+        myApp.current = 2;
+        myApp.jb[0].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 2;
+        myApp.jb[1].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 2;
+        myApp.jb[2].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 2;
+        myApp.jb[3].setSelected(true);
+        Assertions.assertTrue(myApp.check());
+
+        // Testa #4 fråga
+        myApp.current = 3;
+        myApp.jb[0].setSelected(true);
+        Assertions.assertTrue(myApp.check());
+        myApp.current = 3;
+        myApp.jb[1].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 3;
+        myApp.jb[2].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 3;
+        myApp.jb[3].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+
+        // Testa #5 fråga
+        myApp.current = 4;
+        myApp.jb[0].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 4;
+        myApp.jb[1].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 4;
+        myApp.jb[2].setSelected(true);
+        Assertions.assertTrue(myApp.check());
+        myApp.current = 4;
+        myApp.jb[3].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+
+        // Testa #6 fråga
+        myApp.current = 5;
+        myApp.jb[0].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 5;
+        myApp.jb[1].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 5;
+        myApp.jb[2].setSelected(true);
+        Assertions.assertTrue(myApp.check());
+        myApp.current = 5;
+        myApp.jb[3].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+
+        // Testa #7 fråga
+        myApp.current = 6;
+        myApp.jb[0].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 6;
+        myApp.jb[1].setSelected(true);
+        Assertions.assertTrue(myApp.check());
+        myApp.current = 6;
+        myApp.jb[2].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 6;
+        myApp.jb[3].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+
+        // Testa #8 fråga
+        myApp.current = 7;
+        myApp.jb[0].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 7;
+        myApp.jb[1].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 7;
+        myApp.jb[2].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 7;
+        myApp.jb[3].setSelected(true);
+        Assertions.assertTrue(myApp.check());
+
+        // Testa #9 fråga
+        myApp.current = 8;
+        myApp.jb[0].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 8;
+        myApp.jb[1].setSelected(true);
+        Assertions.assertTrue(myApp.check());
+        myApp.current = 8;
+        myApp.jb[2].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 8;
+        myApp.jb[3].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+
+        // Testa #10 fråga
+        myApp.current = 9;
+        myApp.jb[0].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 9;
+        myApp.jb[1].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+        myApp.current = 9;
+        myApp.jb[2].setSelected(true);
+        Assertions.assertTrue(myApp.check());
+        myApp.current = 9;
+        myApp.jb[3].setSelected(true);
+        Assertions.assertFalse(myApp.check());
+
+
+
 
     }
 
