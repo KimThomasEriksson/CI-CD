17ae8c904884999bb6cd363886f572471e81bc3a
SKADAD <32932385+SKADAD@users.noreply.github.com>
2018-11-12 15:47:38 +0100

    Small changes

--- src/Questions.java
+++ src/Questions.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Questions {
 
-    ArrayList<String> questions ;
+    public ArrayList<String> questions ;
 
     public Questions() {
 
@@ -16,8 +16,8 @@ public class Questions {
         questions.add("Que4: String class is defined in which package?");
         questions.add("Que5: Which institute is best for java coaching?");
         questions.add("Que6: Which one among these is not a keyword?");
-        questions.add("Que7: Which one among these is not a class?");
-        questions.add("Que8: which one among these is not a function of Object class?");
+        questions.add("Fråga 7: Vilken av dessa är en klass?");
+        questions.add("Que8: Vilken av dessa är inte en funktion av objekt klass");
         questions.add("Que9: which function is not present in Applet class?");
         questions.add("Que10: Which one among these is not a valid component?");
 
