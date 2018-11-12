e0c8e5f46bc16ef02d4049db94fcd6f5756b3b28
buoc1701 <buoc1701@gmail.com>
2018-11-12 15:44:20 +0100

    Merge branch 'master' of https://github.com/KimThomasEriksson/CI-CD into buoc1701
    # Conflicts:
    #	src/test/OnlineTestTest.java

--- src/Questions.java
+++ src/Questions.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Questions {
 
-    ArrayList<String> questions ;
+    public ArrayList<String> questions ;
 
     public Questions() {
 
@@ -13,8 +13,8 @@ public class Questions {
         questions.add("Que1: Which one among these is not a primitive datatype?");
         questions.add("Que2: Which class is available to all the class automatically?");
         questions.add("Que3: Which package is directly available to our class without importing it?");
-        questions.add("Que4: String class is defined in which package?");
-        questions.add("Que5: Which institute is best for java coaching?");
+        questions.add("Fråga4: Strängklass definieras i vilket paket?");
+        questions.add("Fråga5: Vilket institut är bäst för java coaching?");
         questions.add("Que6: Which one among these is not a keyword?");
         questions.add("Que7: Which one among these is not a class?");
         questions.add("Que8: which one among these is not a function of Object class?");
