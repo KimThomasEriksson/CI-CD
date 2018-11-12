40e17ae83bdc410ce5e248b4710b13119341e41b
Kim Eriksson <32932875+KimThomasEriksson@users.noreply.github.com>
2018-11-12 16:00:34 +0100

    Translated
    Translated questions

--- src/Questions.java
+++ src/Questions.java
@@ -13,10 +13,10 @@ public class Questions {
 
         questions.add("Fråga1: Vilka av dessa är INTE en primitiv datatyp");
         questions.add("Fråga2: Vilken class är tillgänglig för alla klasser automatiskt?");
-        questions.add("Que3: Which package is directly available to our class without importing it?");
+        questions.add("Fråga3: Vilket paket är tillgängligt för våran klass utan att importera den?");
         questions.add("Fråga4: Strängklass definieras i vilket paket?");
         questions.add("Fråga5: Vilket institut är bäst för java coaching?");
-        questions.add("Que6: Which one among these is not a keyword?");
+        questions.add("Fråga6: Vilka av dessa är inte ett nyckelord?");
         questions.add("Fråga 7: Vilken av dessa är en klass?");
         questions.add("Fråga 8: Vilken av dessa är inte en funktion av objekt klass");
         questions.add("Fråga9: Vilken funktion finns inte i Applet-klassen?");
