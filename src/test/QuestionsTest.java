c9fe835245c75ae8f62739732cede5f252c51e40
Kim Eriksson <32932875+KimThomasEriksson@users.noreply.github.com>
2018-11-12 15:59:57 +0100

    Update QuestionsTest.java
    Translated last questions

--- src/test/QuestionsTest.java
+++ src/test/QuestionsTest.java
@@ -15,10 +15,10 @@ class QuestionsTest {
 
         assertEquals("Fråga1: Vilka av dessa är INTE en primitiv datatyp", question.questions.get(0));
         assertEquals("Fråga2: Vilken class är tillgänglig för alla klasser automatiskt?", question.questions.get(1));
-        assertEquals("Que3: Which package is directly available to our class without importing it?", question.questions.get(2));
+        assertEquals("Fråga3: Vilket paket är tillgängligt för våran klass utan att importera den?", question.questions.get(2));
         assertEquals("Fråga4: Strängklass definieras i vilket paket?", question.questions.get(3));
         assertEquals("Fråga5:  Vilket institut är bäst för java coaching?", question.questions.get(4));
-        assertEquals("Que6: Which one among these is not a keyword?", question.questions.get(5));
+        assertEquals("Fråga6: Vilka av dessa är inte ett nyckelord?", question.questions.get(5));
         assertEquals("Fråga 7: Vilken av dessa är en klass?", question.questions.get(6));
         assertEquals("Fråga 8: Vilken av dessa är inte en funktion av objekt klass", question.questions.get(7));
         assertEquals("Fråga9: Vilken funktion finns inte i Applet-klassen?", question.questions.get(8));
