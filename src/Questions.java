51ac8a8b04efdcce608e017ef5791fd0b482fc39
SKADAD <32932385+SKADAD@users.noreply.github.com>
2018-11-12 15:53:24 +0100

    Update Questions.java

--- src/Questions.java
+++ src/Questions.java
@@ -19,10 +19,6 @@ public class Questions {
         questions.add("Que6: Which one among these is not a keyword?");
         questions.add("Fråga 7: Vilken av dessa är en klass?");
         questions.add("Fråga 8: Vilken av dessa är inte en funktion av objekt klass");
-        questions.add("Que9: which function is not present in Applet class?");
-        questions.add("Que10: Which one among these is not a valid component?");
-        questions.add("Que7: Which one among these is not a class?");
-        questions.add("Que8: which one among these is not a function of Object class?");
         questions.add("Fråga9: Vilken funktion finns inte i Applet-klassen?");
         questions.add("Fråga10: Vilken av dessa är inte en giltig komponent?");
 
