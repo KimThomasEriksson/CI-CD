0381829b3f668f49ca6550a919cbfdbd412123b0
buoc1701 <buoc1701@gmail.com>
2018-11-12 09:32:26 +0100

    Test package and file structure redone as template

---
+++ src/test/OnlineTestTest.java
@@ -0,0 +1,22 @@
+package src.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OnlineTestTest {
+
+    @org.junit.jupiter.api.Test
+    void actionPerformed() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void set() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void check() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
